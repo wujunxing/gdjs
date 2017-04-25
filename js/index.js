2f7904f201d68e48b6bcc0887706644da9f08c97
wujunxing <jmwujunxing@gmail.com>
2017-04-26 00:37:33 +0800

    0425

---
+++ js/index.js
@@ -0,0 +1,11 @@
+﻿//项目展示---------------------------------------------------------------Begin
+
+$(function () {
+    $("#product_carousel").tinycarousel({ interval: true });
+    var tinycarousel_data = $("#product_carousel").data("plugin_tinycarousel");
+    $("#product_carousel").hover(
+        function () { tinycarousel_data.stop(); }, function () { tinycarousel_data.start(); }
+    )
+});
+
+//项目展示---------------------------------------------------------------End
